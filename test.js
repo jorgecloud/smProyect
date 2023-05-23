faf2c849e3e778bc556d391eedb04d2c9da4cb26
JorgeEscala <JorgeEscala@gmail.com>
2023-05-23 10:20:55 -0400

    delete1

--- test.js
+++ test.js
@@ -4,9 +4,8 @@ const client = require('twilio')(accountSid, authToken);
 client.messages 
       .create({  
         body: 'hola desde nodejs again', 
-        from: '+13257701298',
-        // messagingServiceSid: 'MGaa7ca78cbdbab6d4be0846cabe4cb073',      
-         to: '+17864316969' 
+        from: '',   
+         to: '' 
        }) 
       .then(data => console.log(data.sid)) 
       .done();
